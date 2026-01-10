7b0895d6f55cae7ca4d772dc871437fc5998b381
Damiana-Arangio <arangio.damiana@gmail.com>
2026-01-10 16:35:28 +0100

    feat(snack 2): arrow function che calcola il quadrato di un numero

---
+++ Snack 2/script.js
@@ -0,0 +1,12 @@
+/* 
+    Crea una arrow function che calcola il quadrato di un numero.
+    Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+*/
+
+
+/* Funzione */
+const quadrato = n => n*n;
+
+/* Output */
+console.log(quadrato(2));
+
